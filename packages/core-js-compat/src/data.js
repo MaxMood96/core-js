92360aafd8f80bc60edeb2f134a17acbd338fc31
Denis Pushkarev <zloirock@zloirock.ru>
2020-11-18 06:19:47 +0700

    remove `esnext.aggregate-error` and `esnext.promise.any` (moved to stable es)

--- packages/core-js-compat/src/data.js
+++ packages/core-js-compat/src/data.js
@@ -1411,8 +1411,6 @@ const data = {
     safari: '10.0',
     rhino: '1.7.13',
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.aggregate-error': null,
   'esnext.array.at': {
     chrome: '92',
     firefox: '90',
@@ -1581,8 +1579,6 @@ const data = {
   },
   'esnext.observable': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.promise.any': null,
   'esnext.promise.try': {
   },
   'esnext.reflect.define-metadata': {
@@ -1741,9 +1737,5 @@ const data = {
     safari: '14.0',
   },
 };
-
-// TODO: Remove from `core-js@4`
-data['esnext.aggregate-error'] = data['es.aggregate-error'];
-data['esnext.promise.any'] = data['es.promise.any'];
 
 module.exports = data;
