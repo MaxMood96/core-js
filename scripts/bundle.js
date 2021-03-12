7585f86b1d14d841035d78800bfbe8b2283dfb17
Denis Pushkarev <zloirock@zloirock.ru>
2021-03-12 21:24:40 +0700

    make `core-js` an equal of `core-js/actual`, generate bundles for `core-js/full` and `core-js/actual`

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -4,6 +4,7 @@ const { cyan, green } = require('chalk');
 const { minify } = require('terser');
 const builder = require('core-js-builder');
 const { banner } = require('core-js-builder/config');
+const actual = require('core-js-compat/entries')['core-js/actual'];
 
 const DENO = process.argv.includes('--deno');
 
@@ -15,13 +16,13 @@ function log(kind, name, code) {
   console.log(green(`${ kind }: ${ cyan(`${ PATH }${ name }.js`) }, size: ${ cyan(`${ size }KB`) }`));
 }
 
-async function bundle({ bundled, minified, options = {} }) {
-  const source = await builder(options);
+async function bundle({ name, min, ...options }) {
+  const source = await builder({ filename: `${ PATH }${ name }.js`, ...options });
+  log('bundling', name, source);
 
-  log('bundling', bundled, source);
-  await writeFile(`${ PATH }${ bundled }.js`, source);
+  if (!min) return;
 
-  if (!minified) return;
+  const minified = `${ name }.min`;
 
   const { code, map } = await minify(source, {
     ecma: 5,
@@ -40,43 +41,18 @@ async function bundle({ bundled, minified, options = {} }) {
       webkit: false,
     },
     sourceMap: {
-      url: `${ minified }.js.map`,
+      url: `${ minified }.map`,
     },
   });
 
   await writeFile(`${ PATH }${ minified }.js`, code);
-  await writeFile(`${ PATH }${ minified }.js.map`, map);
+  await writeFile(`${ PATH }${ minified }.map`, map);
   log('minification', minified, code);
 }
 
-bundle(DENO ? {
-  bundled: 'index',
-  options: {
-    targets: { deno: '1.0' },
-    exclude: [
-      'esnext.aggregate-error',        // moved to stable ES
-      'esnext.global-this',            // moved to stable ES
-      'esnext.map.update-or-insert',   // obsolete
-      'esnext.map.upsert',             // obsolete
-      'esnext.math.iaddh',             // withdrawn
-      'esnext.math.imulh',             // withdrawn
-      'esnext.math.isubh',             // withdrawn
-      'esnext.math.seeded-prng',       // changing of the API, waiting for the spec text
-      'esnext.math.umulh',             // withdrawn
-      'esnext.object.iterate-entries', // withdrawn
-      'esnext.object.iterate-keys',    // withdrawn
-      'esnext.object.iterate-values',  // withdrawn
-      'esnext.promise.all-settled',    // moved to stable ES
-      'esnext.promise.any',            // moved to stable ES
-      'esnext.string.at',              // withdrawn
-      'esnext.string.match-all',       // moved to stable ES
-      'esnext.string.replace-all',     // moved to stable ES
-      'esnext.symbol.pattern-match',   // is not a part of actual proposal, replaced by esnext.symbol.matcher
-      'esnext.symbol.replace-all',     // obsolete
-      'esnext.weak-map.upsert',        // obsolete
-    ],
-  },
-} : {
-  bundled: 'index',
-  minified: 'minified',
-});
+if (DENO) {
+  bundle({ name: 'index', min: false, targets: { deno: '1.0' } });
+} else {
+  bundle({ name: 'full', min: true });
+  bundle({ name: 'actual', min: true, modules: actual });
+}
