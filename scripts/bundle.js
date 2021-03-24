f5d4577efc3cd6dd2345ae90a65cd9a6074532f3
Denis Pushkarev <zloirock@zloirock.ru>
2021-03-24 22:29:57 +0700

    drop non-minified bundles from `core-js-bundle` package

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -3,13 +3,12 @@ const builder = require('@core-js/builder');
 const actual = require('@core-js/compat/entries')['core-js/actual'];
 
 const DENO = process.argv.includes('--deno');
+const PATH = './packages/core-js-bundle/';
 
 if (DENO) {
   builder({ filename: './deno/corejs/full.js', targets: { deno: '1.0' }, minify: false, summary: { size: true } });
 } else {
-  const PATH = './packages/core-js-bundle/';
-  builder({ filename: `${ PATH }full.js`, minify: false, summary: { size: true } });
-  builder({ filename: `${ PATH }full.min.js`, summary: { size: true } });
-  builder({ filename: `${ PATH }actual.js`, modules: actual, minify: false, summary: { size: true } });
-  builder({ filename: `${ PATH }actual.min.js`, modules: actual, summary: { size: true } });
+  builder({ filename: './tests/bundles/core-js.js', minify: false, summary: { size: true } });
+  builder({ filename: `${ PATH }full.js`, summary: { size: true } });
+  builder({ filename: `${ PATH }actual.js`, modules: actual, summary: { size: true } });
 }
